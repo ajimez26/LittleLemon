8fe7479d4c206becfebc3cb5be48df4803a2bf11
ajimez26 <amadiaz622@gmail.com>
2026-01-28 15:56:41 -0500

    Adding changes to Menu and Booking models, updating templates and adding Little lemon database as well

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Booking (models.Model):
+    id = models.IntegerField(primary_key=True,default= 11)
+    name = models.CharField(max_length=255)
+    no_of_guests = models.IntegerField(default=6)
+    BookingDate = models.DateTimeField
+
+class Menu (models.Model):
+    id = models.IntegerField(primary_key= True, default=5)
+    title = models.CharField(max_length=255)
